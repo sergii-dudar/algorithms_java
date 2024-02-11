181edc53a456c2fb752350d64540125dadda889e
Serhii.Dudar <sergey.dudar@sweetcode.pro>
2024-02-12 00:35:01 +0200

    leetcode init

---
+++ leetcode/editor/en/[1]Two Sum.java
@@ -0,0 +1,94 @@
+//Given an array of integers nums and an integer target, return indices of the 
+//two numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// Only one valid answer exists. 
+// 
+//
+// 
+//Follow-up: Can you come up with an algorithm that is less than 
+//O(n²)
+// time complexity?
+//
+// Related Topics Array Hash Table 👍 54408 👎 1829
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SolutionTwoSum {
+
+    public int[] twoSumSol1(int[] nums, int target) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[]{j, i};
+                }
+            }
+        }
+        return new int[2];
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        final Map<Integer, Integer> map = new HashMap<>();
+
+        final int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            final int current = nums[i];
+            final int expectedSecond = target - current;
+
+            final Integer secondIndex;
+            if ((secondIndex = map.get(expectedSecond)) != null) {
+                return new int[]{i, secondIndex};
+            }
+
+            map.put(current, i);
+        }
+
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new SolutionTwoSum().twoSum(
+            new int[]{3, 2, 4}, 5
+        )));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/en/[21]Merge Two Sorted Lists.java
@@ -0,0 +1,123 @@
+//You are given the heads of two sorted linked lists list1 and list2. 
+//
+// Merge the two lists into one sorted list. The list should be made by 
+//splicing together the nodes of the first two lists. 
+//
+// Return the head of the merged linked list. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: list1 = [1,2,4], list2 = [1,3,4]
+//Output: [1,1,2,3,4,4]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: list1 = [], list2 = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: list1 = [], list2 = [0]
+//Output: [0]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in both lists is in the range [0, 50]. 
+// -100 <= Node.val <= 100 
+// Both list1 and list2 are sorted in non-decreasing order. 
+// 
+//
+// Related Topics Linked List Recursion 👍 21041 👎 2001
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode mergeTwoLists(final ListNode listNode1, final ListNode listNode2) {
+
+        ListNode workNode1 = listNode1;
+        ListNode workNode2 = listNode2;
+
+        while (workNode2 != null) {
+
+            final ListNode currentNode2 = new ListNode(workNode2.val, null);
+
+            while (workNode1 != null) {
+                if (currentNode2.val > workNode1.val &&
+                    (workNode1.next != null && currentNode2.val <= workNode1.next.val)) {
+                    swapGrElements(workNode1, currentNode2);
+                }
+
+                workNode1 = workNode1.next;
+            }
+
+            workNode2 = workNode2.next;
+            workNode1 = listNode1;
+        }
+
+        return workNode1;
+    }
+
+    private static void swapGrElements(ListNode el1, ListNode el2) {
+        final ListNode next = el1.next;
+        el1.next = el2;
+        el2.next = next;
+    }
+
+    public static void main(String[] args) {
+        final ListNode list1 = toLinkedList(List.of(1, 2, 4, 6, 7));
+        final ListNode list2 = toLinkedList(List.of(1, 3, 4, 5, 7, 9, 11, 14));
+
+        System.out.println("list1 is: " + toArrayList(list1));
+        System.out.println("list2 is: " + toArrayList(list2));
+
+        final ListNode result = new Solution().mergeTwoLists(list1, list2);
+
+        System.out.println("result   is: " + toArrayList(result));
+        System.out.println("expected is: [1, 1, 2, 3, 4, 4, 5, 6, 7, 7 , 9, 11, 14]");
+
+    }
+
+    public static ListNode toLinkedList(List<Integer> list) {
+        ListNode node = null;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            node = new ListNode(list.get(i), node);
+        }
+        return node;
+    }
+
+    public static ArrayList<Integer> toArrayList(ListNode node) {
+        final ArrayList<Integer> result = new ArrayList<>();
+
+        while (node != null) {
+            result.add(node.val);
+            node = node.next;
+        }
+
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
