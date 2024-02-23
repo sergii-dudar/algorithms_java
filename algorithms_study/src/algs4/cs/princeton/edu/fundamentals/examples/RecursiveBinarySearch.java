c4a44d571ecbda9ad1a81a222eb3f23dfdc2a687
sergey.dudar <sergey.dudar@sweetcode.pro>
2024-02-23 19:19:07 +0200

    algo study fundam

---
+++ algorithms_study/src/algs4/cs/princeton/edu/fundamentals/examples/RecursiveBinarySearch.java
@@ -0,0 +1,21 @@
+package algs4.cs.princeton.edu.fundamentals.examples;
+
+public class RecursiveBinarySearch {
+
+    public static void main(String[] args) {
+        System.out.println(rank(5, new int[] {1,4,5,6,7,4,3}));
+    }
+
+    public static int rank(int key, int[] a) {
+        return rank(key, a, 0, a.length -  1);
+    }
+
+    public static int rank(int key, int[] a, int lo, int hi) { // Index of key in a[], if present, is not smaller than lo
+// and not larger than hi.
+        if (lo > hi) return -1;
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) return rank(key, a, lo, mid - 1);
+        else if (key > a[mid]) return rank(key, a, mid + 1, hi);
+        else return mid;
+    }
+}
